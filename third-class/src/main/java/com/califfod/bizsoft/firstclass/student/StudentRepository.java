34aef6e449eb03b8548e1320471e5786db2fec2f
Nayeem Biswas <43644126+nayeembiswas@users.noreply.github.com>
2024-03-26 22:38:06 +0600

    so

--- third-class/src/main/java/com/califfod/bizsoft/firstclass/student/StudentRepository.java
+++ third-class/src/main/java/com/califfod/bizsoft/firstclass/student/StudentRepository.java
@@ -2,5 +2,11 @@ package com.califfod.bizsoft.firstclass.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    public Optional<Student> findByUserId(Integer userId);
+
+    public List<Student> findByNameContaining(String name);
 }
